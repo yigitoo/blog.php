329ff005c44507c80e9f609f2fc9f50cd9a3d61d
Yiğit GÜMÜŞ <rawns0909@gmail.com>
2023-02-06 23:23:02 +0300

    .

---
+++ src/footer.php
@@ -0,0 +1,5 @@
+<div class="footer">
+	<center><h1 style="color: white;">Created by <a href="https://github.com/yigitoo/blog.php">yigitoo</a>.</h1></center>
+</div>
+</body>
+</html>
---
+++ src/header.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <title><?php echo $title; ?></title>
+    <link rel="stylesheet" type="text/css" href="css.css">
+    <style type="text/css">
+    	.header
+    	{
+    		width: 100%;
+    		height: 10%;
+    		background-color: lightgrey;
+    	}
+		.footer
+		{
+    		width: 100%;
+    		height: 10%;
+    		background-color: green;
+    		position: absolute;
+    		bottom: 0%;
+    	}
+    </style>
+</head>
+<body>
+<div class="navbar">
+	<center><h1 style="color: black;">This is the header.</h1></center>
+</div>
